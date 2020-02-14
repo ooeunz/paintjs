52eb58a8ac8be78f8859f9c28aaaed04d5658c76
ooeunz <yuns994@gmail.com>
2020-02-14 18:29:14 +0900

    make canvas and drak stroke only black color

---
+++ app.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeSyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+const stopPainting = () => {
+  painting = false;
+}
+
+const startPainting = () => {
+  painting = true;
+}
+
+const onMouseMove = (event) => {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+} 
+
+const onMouseDown = (event) => {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
