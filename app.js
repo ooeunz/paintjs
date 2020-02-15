b3a1fb98fc69c6e54da872658e81eefcaed0b505
ooeunz <yuns994@gmail.com>
2020-02-15 15:05:17 +0900

    Saving the Img

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const DEFAULT_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,7 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
 ctx.strokeStyle = DEFAULT_COLOR;
 ctx.fillStyle = DEFAULT_COLOR;
 ctx.lineWidth = 2.5;
@@ -66,12 +68,26 @@ const handleCanvasClick = (event) => {
   }
 }
 
+const handleCM = (event) => {
+  event.preventDefault();
+}
+
+const handleSaveClick = (event) => {
+  const img = canvas.toDataURL();
+  const link = document.createElement("a");
+  
+  link.href = img
+  link.download = "PaingJS[🎨]";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -82,4 +98,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick)
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick)
 }
